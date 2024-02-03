b3d6757a5f50581e1a8b9641b90b93999a22b70c
Dimitar Lilov <devilerser@gmail.com>
2024-02-03 19:23:21 +0200

    First Implementation of RMT - used from drv_dht11 for DHT11 sensor

--- drv_dht11.c
+++ drv_dht11.c
@@ -13,20 +13,49 @@
  **************************************************************************** */
 #include "drv_dht11.h"
 
+#include "esp_log.h"
 #include "esp_timer.h"
 #include "driver/gpio.h"
 #include "rom/ets_sys.h"
+#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+
 
 /* *****************************************************************************
  * Configuration Definitions
  **************************************************************************** */
 #define TAG "drv_dht11"
 
+/* priority high to low */
+#define USE_RMT             1
+#define USE_GPIO_INTERRUPT  0
+
+
+
+
+
+#if USE_RMT
+#undef USE_GPIO_INTERRUPT
+#define USE_GPIO_INTERRUPT  0
+
+#define USE_RMT_RECONFIGURE_EACH_TRANSMISSION   0
+#endif
+
+
+#if USE_RMT
+#if CONFIG_DRV_RMT_USE
+#include "drv_rmt.h"
+#else
+#include "driver/rmt.h"
+#endif
+#endif
+
 /* *****************************************************************************
  * Constants and Macros Definitions
  **************************************************************************** */
+#define COUNT_INTERRUPTS_PER_TRANSMISSION_MAX   100
 
 /* *****************************************************************************
  * Enumeration Definitions
@@ -47,6 +76,15 @@ static gpio_num_t dht_gpio;
 static int64_t last_read_time = -2000000;
 static struct drv_dht11_reading last_read;
 
+#if USE_GPIO_INTERRUPT
+static bool b_interrupt_registered = false;
+static int interrupts_count = 0;
+static bool b_skip_dht_response_first_bit = false;
+//static uint8_t pin_level[COUNT_INTERRUPTS_PER_TRANSMISSION_MAX] = {0};
+static int delay_edge_change[COUNT_INTERRUPTS_PER_TRANSMISSION_MAX] = {0};
+static int64_t last_interrupt_read_time = 0;
+#endif
+
 /* *****************************************************************************
  * Prototype of functions definitions
  **************************************************************************** */
@@ -55,61 +93,171 @@ static struct drv_dht11_reading last_read;
  * Functions
  **************************************************************************** */
 
-static int _waitOrTimeout(uint16_t microSeconds, int level) {
-    int micros_ticks = 0;
-    while(gpio_get_level(dht_gpio) == level) { 
-        if(micros_ticks++ > microSeconds) 
-            return DRV_DHT11_TIMEOUT_ERROR;
-        ets_delay_us(1);
-    }
-    return micros_ticks;
-}
+// static int _waitOrTimeout(uint16_t microSeconds, int level) {
+//     int micros_ticks = 0;
+//     while(gpio_get_level(dht_gpio) == level) { 
+//         if(micros_ticks++ > microSeconds) 
+//             return DRV_DHT11_TIMEOUT_ERROR;
+//         ets_delay_us(1);
+//     }
+//     return micros_ticks;
+// }
 
 static int _checkCRC(uint8_t data[]) {
-    if(data[4] == (data[0] + data[1] + data[2] + data[3]))
+    if(data[4] == (uint8_t)(data[0] + data[1] + data[2] + data[3]))
         return DRV_DHT11_OK;
     else
         return DRV_DHT11_CRC_ERROR;
 }
 
+#if USE_GPIO_INTERRUPT
+static void IRAM_ATTR dht11_gpio_isr_handler(void* arg) 
+{
+    int64_t curr_interrupt_read_time = esp_timer_get_time();
+    // Handle interrupt here
+    //ESP_LOGI(TAG, "%d", gpio_get_level(dht_gpio));
+    //pin_level[interrupts_count] = gpio_get_level(dht_gpio);
+
+    if (b_skip_dht_response_first_bit)
+    {
+        b_skip_dht_response_first_bit = false;
+    }
+    else
+    {
+        delay_edge_change[interrupts_count] = curr_interrupt_read_time - last_interrupt_read_time;
+        interrupts_count++;
+    }
+
+    last_interrupt_read_time = curr_interrupt_read_time;
+}
+#endif
+
+
+
 static void _sendStartSignal() {
-    gpio_set_direction(dht_gpio, GPIO_MODE_OUTPUT);
+    gpio_set_direction(dht_gpio, GPIO_MODE_INPUT_OUTPUT_OD);
+
+    #if USE_GPIO_INTERRUPT
+    if (b_interrupt_registered == false)
+    {
+        gpio_install_isr_service(ESP_INTR_FLAG_SHARED); // Choose an appropriate interrupt flag
+        gpio_set_intr_type(dht_gpio, GPIO_INTR_NEGEDGE);
+        gpio_isr_handler_add(dht_gpio, dht11_gpio_isr_handler, (void*) dht_gpio);
+        b_interrupt_registered = true;
+    }
+    #endif
+
+
+    #if USE_RMT
+    #if CONFIG_DRV_RMT_USE
+    #if USE_RMT_RECONFIGURE_EACH_TRANSMISSION
+    drv_rmt_test_init_rx(); //test rmt pin and dht_gpio must match
+    #else
+    size_t len = drv_rmt_test_read_rx(NULL, 0, 0); 
+    ESP_LOGD(TAG, "Read Buffer Before Transmission : %d ", len);
+    #endif
+    #else
+    //to do local imlementation of rmt driver
+    #endif
+    #endif
+
+
     gpio_set_level(dht_gpio, 0);
+
+    //vTaskDelay(pdMS_TO_TICKS(20));
     ets_delay_us(20 * 1000);
+
+    #if USE_GPIO_INTERRUPT
+    last_interrupt_read_time = esp_timer_get_time();
+
+    interrupts_count = 0;
+    b_skip_dht_response_first_bit = true;
+    gpio_intr_enable(dht_gpio);
+    #endif
+
+
+    #if USE_RMT
+    #if CONFIG_DRV_RMT_USE
+    #if USE_RMT_RECONFIGURE_EACH_TRANSMISSION
+    drv_rmt_test_start_rx();
+    #else
+    drv_rmt_test_start_rx();
+    //drv_rmt_test_reset_rx();
+    #endif
+    #else
+    //to do local imlementation of rmt driver
+    #endif
+    #endif
+
     gpio_set_level(dht_gpio, 1);
-    ets_delay_us(40);
-    gpio_set_direction(dht_gpio, GPIO_MODE_INPUT);
+
+    //ets_delay_us(40);
 }
 
-static int _checkResponse() {
-    /* Wait for next step ~80us*/
-    if(_waitOrTimeout(80, 0) == DRV_DHT11_TIMEOUT_ERROR)
-        return DRV_DHT11_TIMEOUT_ERROR;
+#if USE_RMT == 0
+static void _completed_transmission()
+{
+    #if USE_GPIO_INTERRUPT
+    gpio_intr_disable(dht_gpio);
+    //gpio_set_direction(dht_gpio, GPIO_MODE_INPUT);
+    ESP_LOGD(TAG, "Count Interrupts %d", interrupts_count);
+    //ESP_LOG_BUFFER_HEX(TAG, pin_level, interrupts_count);
 
-    /* Wait for next step ~80us*/
-    if(_waitOrTimeout(80, 1) == DRV_DHT11_TIMEOUT_ERROR) 
-        return DRV_DHT11_TIMEOUT_ERROR;
-
-    return DRV_DHT11_OK;
+    for (int index = 0; index < interrupts_count; index++)
+    {
+        ESP_LOGD(TAG, "[%2d]=%6d", index, delay_edge_change[index]);
+    }
+    #endif
 }
+#endif
 
-static struct drv_dht11_reading _timeoutError() {
-    struct drv_dht11_reading timeoutError = {DRV_DHT11_TIMEOUT_ERROR, -1, -1};
-    return timeoutError;
-}
+// static int _checkResponse() {
+//     /* Wait for next step ~80us*/
+//     if(_waitOrTimeout(80, 0) == DRV_DHT11_TIMEOUT_ERROR)
+//         return DRV_DHT11_TIMEOUT_ERROR;
 
-static struct drv_dht11_reading _crcError() {
-    struct drv_dht11_reading crcError = {DRV_DHT11_CRC_ERROR, -1, -1};
-    return crcError;
-}
+//     /* Wait for next step ~80us*/
+//     if(_waitOrTimeout(80, 1) == DRV_DHT11_TIMEOUT_ERROR) 
+//         return DRV_DHT11_TIMEOUT_ERROR;
 
-void drv_dht11_init(gpio_num_t gpio_num) {
+//     return DRV_DHT11_OK;
+// }
+
+// static struct drv_dht11_reading _timeoutError() {
+//     struct drv_dht11_reading timeoutError = {DRV_DHT11_TIMEOUT_ERROR, -1, -1};
+//     return timeoutError;
+// }
+
+// static struct drv_dht11_reading _crcError() {
+//     struct drv_dht11_reading crcError = {DRV_DHT11_CRC_ERROR, -1, -1};
+//     return crcError;
+// }
+
+void drv_dht11_init(gpio_num_t gpio_num) 
+{
+    esp_log_level_set(TAG, ESP_LOG_INFO);
     /* Wait 1 seconds to make the device pass its initial unstable status */
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     dht_gpio = gpio_num;
+
+
+    #if USE_RMT
+    #if CONFIG_DRV_RMT_USE
+    #if USE_RMT_RECONFIGURE_EACH_TRANSMISSION
+    #else
+    drv_rmt_test_init_rx(); //test rmt pin and dht_gpio must match
+    drv_rmt_test_start_rx();
+    #endif
+    #else
+    //to do local imlementation of rmt driver
+    #endif
+    #endif
+
+
 }
 
-struct drv_dht11_reading drv_dht11_read() {
+struct drv_dht11_reading drv_dht11_read() 
+{
     /* Tried to sense too soon since last read (dht11 needs ~2 seconds to make a new read) */
     if(esp_timer_get_time() - 2000000 < last_read_time) {
         return last_read;
@@ -121,20 +269,103 @@ struct drv_dht11_reading drv_dht11_read() {
 
     _sendStartSignal();
 
-    if(_checkResponse() == DRV_DHT11_TIMEOUT_ERROR)
-        return last_read = _timeoutError();
+    // if(_checkResponse() == DRV_DHT11_TIMEOUT_ERROR)
+    //     return last_read = _timeoutError();
     
-    /* Read response */
-    for(int i = 0; i < 40; i++) {
-        /* Initial data */
-        if(_waitOrTimeout(50, 0) == DRV_DHT11_TIMEOUT_ERROR)
-            return last_read = _timeoutError();
+    // /* Read response */
+    // for(int i = 0; i < 40; i++) {
+    //     /* Initial data */
+    //     if(_waitOrTimeout(50, 0) == DRV_DHT11_TIMEOUT_ERROR)
+    //         return last_read = _timeoutError();
                 
-        if(_waitOrTimeout(70, 1) > 28) {
-            /* Bit received was a 1 */
-            data[i/8] |= (1 << (7-(i%8)));
-        }
+    //     if(_waitOrTimeout(70, 1) > 28) {
+    //         /* Bit received was a 1 */
+    //         data[i/8] |= (1 << (7-(i%8)));
+    //     }
+    // }
+
+
+
+
+    #if USE_RMT == 0
+    ets_delay_us(5 * 1000 + 1000); //at least > 4.8 ms (120*40 + 180)
+    _completed_transmission();
+    gpio_set_direction(dht_gpio, GPIO_MODE_INPUT);
+    #endif
+
+
+    #if USE_RMT
+    #if CONFIG_DRV_RMT_USE
+    size_t len = 0;
+
+    //len = drv_rmt_test_read_rx(data, sizeof(data), pdMS_TO_TICKS(5+1)+1); //at least one rtos tick
+    len = drv_rmt_test_read_rx(data, sizeof(data), portMAX_DELAY); //at least one rtos tick
+
+
+    #if USE_RMT_RECONFIGURE_EACH_TRANSMISSION
+    drv_rmt_test_stop_rx();
+    drv_rmt_test_deinit_rx();
+    #else
+    drv_rmt_test_stop_rx();
+    #endif
+
+    if (len)
+    {
+        ESP_LOG_BUFFER_HEX_LEVEL(TAG, data, len, ESP_LOG_DEBUG);
     }
+    else
+    {
+        ESP_LOGE(TAG, "Timeout RMT");
+    }
+
+
+
+
+
+    // ets_delay_us(10000);
+    // gpio_set_level(dht_gpio, 0);
+    // ets_delay_us(1000);
+    // gpio_set_level(dht_gpio, 1);
+
+    
+
+    #else
+    //to do local imlementation of rmt driver
+    #endif
+
+    gpio_set_direction(dht_gpio, GPIO_MODE_INPUT);
+    
+    #endif
+
+
+#if USE_GPIO_INTERRUPT
+    if (interrupts_count == 40)
+    {
+        for (int index = 0; index < interrupts_count; index++)
+        {
+            if(delay_edge_change[index] >= 120)   //50+28 for 0 | 50+70 for 1
+            {
+                /* Bit received was a 1 */
+                data[index/8] |= (1 << (7-(index%8)));
+            }
+        }
+        ESP_LOG_BUFFER_HEX(TAG, data, 5);
+    }
+    else
+#endif
+#if USE_RMT
+    if (len == 5)
+    {
+
+    }
+    else
+#endif
+    {
+        //last_read = _timeoutError();
+        last_read.status = DRV_DHT11_TIMEOUT_ERROR;
+        return last_read;
+    }
+    
 
     if(_checkCRC(data) != DRV_DHT11_CRC_ERROR) {
         last_read.status = DRV_DHT11_OK;
@@ -142,6 +373,12 @@ struct drv_dht11_reading drv_dht11_read() {
         last_read.humidity = data[0];
         return last_read;
     } else {
-        return last_read = _crcError();
+        //last_read = _crcError();
+        last_read.status = DRV_DHT11_CRC_ERROR;
+        //last_read.temperature = data[2];
+        //last_read.humidity = data[0];
+        return last_read;
     }
 }
+
+
